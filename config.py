0bebbf6ef23f1cf5331a9d21c84dc880142945e3
Bloodlinebots <githubsbots@gmail.com>
2025-06-07 13:29:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@
 
 import os
 from dotenv import load_dotenv
-
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-DEEPAI_API_KEY = os.getenv("DEEPAI_API_KEY")
-LOG_CHANNEL_ID = os.getenv("LOG_CHANNEL_ID")
+SIGHTENGINE_USER = os.getenv("SIGHTENGINE_USER")
+SIGHTENGINE_SECRET = os.getenv("SIGHTENGINE_SECRET")
+LOG_CHANNEL_ID = int(os.getenv("LOG_CHANNEL_ID", 0))
 CLEANUP_DELAY_SECONDS = int(os.getenv("CLEANUP_DELAY_SECONDS", 900))
